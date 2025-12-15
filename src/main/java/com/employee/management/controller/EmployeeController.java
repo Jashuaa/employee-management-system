629e1dbcde65f79deabdc7dd10e08e5476bd8a94
Jashwanth Reddy <iamjashwanth55@gmail.com>
2025-12-15 15:30:03 +0530

    add CORS annotation for frontend integration

--- src/main/java/com/employee/management/controller/EmployeeController.java
+++ src/main/java/com/employee/management/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import com.employee.management.service.EmployeeService;
 
 import jakarta.validation.Valid;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
